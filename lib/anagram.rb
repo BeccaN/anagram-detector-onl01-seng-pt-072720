436bde5b8b8d565969541cf83501f0c0d1ff23c5
Rebecca Congi <rncongi@gmail.com>
2020-08-19 19:29:38 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,28 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word
+  end 
+  
+  def match(words_array)
+    
+    words_array.select do |word|
+      word.split("").sort == @word.split("").sort
+    end 
+
+  end 
+  
+  # def match(words_array)
+  #   x = []
+  #   words_array.each do |word|
+  #     if word.split("").sort == @word.split("").sort
+  #       x << word
+  #     end
+  #   end 
+  #   x 
+  # end 
+    
+end   
